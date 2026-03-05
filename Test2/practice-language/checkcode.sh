63c06a102a5973c90699dbc2329546b759b7e702
Matin Qurbanzadeh <61672425+MatinQurban@users.noreply.github.com>
2026-03-04 23:42:24 -0800

    Stackrecursion - language and stretch

---
+++ Test2/practice-language/checkcode.sh
@@ -0,0 +1,5 @@
+# script to check files and run student program on provided sample test cases
+# This script is available to students.
+cd RoboGrader
+python3 robochecker.py StackRecursion "/Users/matinq/Class Programming/UCSC/CSE 101/Test2/practice-language"
+cd ..
